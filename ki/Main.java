c61a43ab5d408c6c9ddcdec677c69602de0f5262
ankitachopade19-lab <ankitachopade19@gmail.com>
2026-02-26 17:24:55 +0530

    Add files via upload

---
+++ ki/Main.java
@@ -0,0 +1,65 @@
+package com.ki;
+
+//public class Main {
+//	static int emp_age = 22;
+//	static int emp_id = 101;
+//	static long emp_Mobno = 8797885463l;
+//	static String emp_name = "Anu";
+//	static String emp_sal = "5LPA";
+//	
+//	public static void main(String args[]) {
+//		
+//		System.out.println("Employee Age = " +emp_age);
+//		System.out.println("Employee id = "+emp_id);
+//		System.out.println("Employee Mobile No. = "+emp_Mobno);
+//		System.out.println("Employee Name = " +emp_name);
+//		System.out.println("Employee Salary = " +emp_sal);
+//	}
+//
+//}
+
+//public class Main {
+//	
+//	static byte age = 20;
+//	static short id = 102;
+//	static int Seat_no = 234566;
+//	static long adhar_no = 324758694657l;
+//	static float marks = 8.9f;
+//	static double percentage = 85.99;
+//	
+//	public static void main(String args[]){
+//		
+//		System.out.println("Student Age : " +age);
+//		System.out.println("Student ID : " +id);
+//		System.out.println("Student seat No. : "+Seat_no);
+//		System.out.println("Student Adhar No. : "+adhar_no);
+//		System.out.println("Student Marks : "+ marks);
+//		System.out.println("Student Percentage : " + percentage);
+//		
+//	}
+//	
+//}
+
+public class Main{
+	
+	static String Bank_name = "State Bank of India";
+	static String branch = "Ghodeshwar";
+	static int Total_acc = 1;
+	static long Acc_no = 237599860l;
+	static String holder_name = "Ankita";
+	static double balance = 50000.00;
+	static int IFSC_code = 124547;
+	
+    public static void main(String args[]) {
+		
+		System.out.println("Bank Name:" + Bank_name);
+		System.out.println("Branch:" + branch);
+		System.out.println("Total Accounts:" + Total_acc);
+		System.out.println("Account No.:" + Acc_no);
+		System.out.println("Holder Name:" + holder_name);
+		System.out.println("Balance:" + balance);
+		System.out.println("IFSC code:" + IFSC_code);
+   }
+    
+}
+
---
+++ ki/NonPrimitive.java
@@ -0,0 +1,14 @@
+package com.ki;
+
+public class NonPrimitive {
+	
+	public static void main(String args[]) {
+		
+		String Name = "Ankita Chopade";
+		String Paragraph = "Everybody who is living on this earth is leading his life, but if we talk about the human beings then their life is not the bed of roses, it has problems and struggles.";
+	    System.out.println("Name : " +Name);
+		System.out.println("Paragraph : "+Paragraph);
+	}
+	
+
+}
