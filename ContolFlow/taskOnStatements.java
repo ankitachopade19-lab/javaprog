9ac9365103c1f0d8099c74ab3dda9daeb142a87a
ankitachopade19-lab <ankitachopade19@gmail.com>
2026-03-06 14:07:21 +0530

    Add files via upload

---
+++ ContolFlow/taskOnStatements.java
@@ -0,0 +1,72 @@
+package ContolFlow;
+
+public class taskOnStatements {
+	
+     public static void main(String[] args) {
+		
+//-----------1. Check Even or Odd Number -----------
+	
+/*		int num = 44;
+		
+		if (num % 2==0) {
+			System.out.println("Even Number : " + num);
+			
+		}else {
+			System.out.println("Odd Number: " + num);
+		}	
+ 
+
+//-------- 2. Check if a Person Can Vote------------------
+         
+         int age = 20;
+         
+         if(age>=18) {
+	       System.out.println("Eligible For Voting...");
+         }else {
+	       System.out.println(" Not Eligible For Voting...");
+         } 
+       
+         
+//---------- 3 . Positive or Negative Number------------
+ 		
+ 		 int num  = 15;
+ 		
+ 		 if(num > 0) {
+ 			 System.out.println("Number is Positive  ");
+ 		 }
+ 		 else {
+ 			System.out.println("Number is Negative  ");
+ 		 }
+		
+//-----------4. Write a program to check whether a number is positive, negative, or zero.-----------
+ 
+ 		 int num  = -98;
+ 			
+ 		 if(num > 0) {
+ 			System.out.println("Number is positive  ");
+ 		 }
+ 		 else if(num < 0) {
+ 				System.out.println("Number is negative  ");
+ 				
+ 		 }else {
+ 				System.out.println("Number is 0  ");
+ 		}
+ */  		 
+ 		 
+//----------5. Write a program to compare two numbers and print the greater one.-------------------
+ 		
+ 		int num1 = 80;
+ 		int num2 = 97;
+ 		
+ 		if(num1 > num2) {
+ 			System.out.println("Num1 is greater than Num2  ");
+ 			
+ 		} else if(num1 < num2){
+ 			System.out.println("Num1 is less than num2  ");
+ 			
+ 		}else {
+ 			System.out.println("Num1 is equals to Num2  ");
+ 		}		
+     }
+}
+ 
